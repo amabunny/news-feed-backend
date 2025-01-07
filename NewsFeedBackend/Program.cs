6ab2dbff0a6ec9bb77e3bf9a14226428c7c59a3f
Sergey <iamgreyy@gmail.com>
2025-01-07 18:54:17 +0500

    fix(program): remove migrations apply on startup

--- NewsFeedBackend/Program.cs
+++ NewsFeedBackend/Program.cs
@@ -27,12 +27,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<NewsDbContext>();
-    context.Database.Migrate();
-}
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
