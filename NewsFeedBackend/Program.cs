c4b72c3d753ba669f17f685fdf0151d063ccef00
Sergey <iamgreyy@gmail.com>
2025-01-07 18:48:17 +0500

    fix(program): apply migrations on startup

--- NewsFeedBackend/Program.cs
+++ NewsFeedBackend/Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<NewsDbContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
